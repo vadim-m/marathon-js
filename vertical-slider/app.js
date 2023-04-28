c5e75e9c1a08dded90a8e133d414933b4f3bd068
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-04-28 21:04:18 +0300

    feat: add vertical-slider logic

---
+++ vertical-slider/app.js
@@ -0,0 +1,40 @@
+"use string";
+
+const upBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
+const sidebar = document.querySelector(".sidebar");
+const mainSlide = document.querySelector(".main-slide");
+const slidesCount = mainSlide.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener("click", () => {
+  changeSlide("up");
+});
+
+downBtn.addEventListener("click", () => {
+  changeSlide("down");
+});
+
+function changeSlide(direction) {
+  if (direction === "up") {
+    activeSlideIndex++;
+
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0;
+    }
+  }
+
+  if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1;
+    }
+  }
+
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * 100}vh)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * 100}vh)`;
+  console.log(activeSlideIndex);
+}
