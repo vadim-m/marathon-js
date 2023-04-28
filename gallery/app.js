af88991d6a6a315e08afe3fdc5d5b381f74851c5
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-04-28 15:12:27 +0300

    feat: add slidesPlugin function

--- gallery/app.js
+++ gallery/app.js
@@ -1,14 +1,21 @@
 "use strict";
-const slides = document.querySelectorAll(".slide");
 
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
+function slidesPlugin(activeSlide = 1) {
+  const slides = document.querySelectorAll(".slide");
 
-    slide.classList.add("active");
-  });
+  slides[activeSlide].classList.add("active");
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+
+      slide.classList.add("active");
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => slide.classList.remove("active"));
+  }
 }
 
-function clearActiveClasses() {
-  slides.forEach((slide) => slide.classList.remove("active"));
-}
+slidesPlugin(1);
